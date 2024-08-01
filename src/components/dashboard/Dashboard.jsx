e09e30a6d340e3700a1018a9f945b186999a9696
Shivansh <greatshiva773@gmail.com>
2024-08-01 20:08:56 +0530

    Add Routes

---
+++ src/components/dashboard/Dashboard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Home from './Home'
+import Sidebar from './Sidebar'
+
+function Dashboard() {
+  return (
+    <div>
+        <Home />
+        <Sidebar />
+    </div>
+  )
+}
+
+export default Dashboard
