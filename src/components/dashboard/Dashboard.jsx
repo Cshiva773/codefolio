d3d341d2b3858fb6681c5b7ec4afc055789b2269
Shivansh <greatshiva773@gmail.com>
2024-08-01 20:15:34 +0530

    Add header component

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Home from './Home'
 import Sidebar from './Sidebar'
+import Header from './Header'
 
 function Dashboard() {
   return (
     <div>
+        <Header />
         <Home />
         <Sidebar />
     </div>
