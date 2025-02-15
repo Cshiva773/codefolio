4133f585dde1c4e0edef0c957760042e1a50a55f
Shivansh <greatshiva773@gmail.com>
2025-02-16 02:22:26 +0530

    ADD USER AND CODINGPROFILE MODEL

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -3,7 +3,7 @@ import { DB_NAME } from '../constants.js'
 
 const connectDB=async () =>{
     try{
-        await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
+        await mongoose.connect(`${process.env.DB_URL}/${DB_NAME}`)
         console.log("Database connected")
         
     }catch(err){
